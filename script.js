9836d5aef5348cf31da42481a7b2556f8c7fb057
sunshineonleaf <84730895+sunshineonleaf@users.noreply.github.com>
2022-08-03 18:27:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,13 @@
-const el = document.querySelector('.heart');
-const heart = $('.heart svg');
-let tl = new TimelineMax({ paused: true });
-let timeline = new mojs.Timeline();
+function addListener() {
+  var btn = document.querySelector('#Heart_2_');
+  btn.addEventListener("click", restart, false);  
+}
 
-tl.add(
-TweenMax.to(heart, 0.15, {
-  scaleX: .4,
-  scaleY: .2,
-  ease: Back.easeOut.config(4) }));
+function restart() {
+  var container = document.querySelector('#Layer_1');
+  var newContainer = container.cloneNode(true);
+  container.parentNode.replaceChild(newContainer, container);
+  addListener();
+}
 
-
-tl.add(
-TweenMax.to(heart, 0.25, {
-  scaleX: 1,
-  scaleY: 1,
-  ease: Back.easeOut.config(4) }));
-
-
-
-const burst = new mojs.Burst({
-  parent: el,
-  count: 10,
-  radius: { 0: 80 },
-  duration: 1500,
-  children: {
-    radius: { 15: 0 },
-    easing: 'cubic.out',
-    degreeShift: 'rand(-50,50)' } });
-
-
-
-const burst2 = new mojs.Burst({
-  parent: el,
-  count: 15,
-  radius: { 0: 60 },
-  children: {
-    shape: 'line',
-    stroke: 'white',
-    fill: 'none',
-    scale: 1,
-    scaleX: { 1: 0 },
-    easing: 'cubic.out',
-    duration: 1000,
-    degreeShift: 'rand(-50, 50)' } });
-
-
-
-const bubbles = new mojs.Burst({
-  parent: el,
-  radius: 50,
-  count: 5,
-  timeline: { delay: 200 },
-  children: {
-    stroke: 'white',
-    fill: 'none',
-    scale: 1,
-    strokeWidth: { 8: 0 },
-    radius: { 0: 'rand(6, 10)' },
-    degreeShift: 'rand(-50, 50)',
-    duration: 400,
-    delay: 'rand(0, 250)' } });
-
-
-
-const circ_opt = {
-  parent: el,
-  radius: { 0: 50 },
-  duration: 750,
-  shape: 'circle',
-  fill: 'none',
-  stroke: '#FF4136',
-  strokeWidth: 1,
-  opacity: { 1: 0 } };
-
-
-const circ = new mojs.Shape({
-  ...circ_opt });
-
-
-const circ2 = new mojs.Shape({
-  ...circ_opt,
-  delay: 100 });
-
-
-timeline.add(circ, circ2);
-
-
-// when clicking the button start the timeline/animation:
-$(el).on('click', function () {
-  if ($(el).hasClass('active')) {
-    $(el).toggleClass('active');
-  } else {
-    $(el).toggleClass('active');
-    tl.restart();
-    burst.
-    generate().
-    replay();
-    burst2.
-    generate().
-    replay();
-    bubbles.
-    generate().
-    replay();
-    timeline.replay();
-  }
-});
+addListener();
